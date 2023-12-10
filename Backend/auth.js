ba822500a699c6dc2b2e94f4578b52039e691038
Surya Nudurupati <nudurupatisurya@gmail.com>
2023-12-10 13:21:14 -0800

    Initial commit

---
+++ Backend/auth.js
@@ -0,0 +1,5 @@
+// auth.js
+
+// TODO: Implement form submission logic
+// TODO: Integrate with AWS Cognito for authentication
+// TODO: Handle login success and failure
---
+++ README.md
@@ -0,0 +1,2 @@
+# Civix
+ One stop destination to report all your city's issues and we will notify the government about them.
